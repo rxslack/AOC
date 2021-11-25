542235ff709ff88748e18df8ea34744843d337ab
Rachael Slack <57686060+rxchxel@users.noreply.github.com>
2021-11-25 08:16:06 +0000

    day 5 completed

--- 2015/day5.js
+++ 2015/day5.js
@@ -6,25 +6,71 @@ function readFile(){
     return file;
 }
 
-//const input = readFile();
-const input = "ugknbfddgicrmopn"
-const inputArray = input.split('\n');
-let niceStringsTotal = 0;
-let vowels = 0;
-let repeats = 0;
-function processString(string){
-
-    const strings = string.split('');
-    for (let i = 0; i < strings.length; i++){
-        if(strings[i] == "a" || strings[i] == "e" || strings[i] =="i" || strings[i] == "o"|| strings[i] == "u"){
-            vowels += 1;
+function vowelCheck(stringArray){
+    let counter = 0;
+    for (let i = 0; i < stringArray.length; i++){
+        const letter = stringArray[i];
+        if(letter == "a" || letter == "e" || letter =="i" || letter == "o"|| letter == "u"){
+            counter += 1;
         }
-
-        if (strings[i] == strings[i+1]){
-            repeats += 1;
-        }
-
     }
+    return counter >=3;
 }
 
-console.log(inputArray);
+function repeatCheck(stringArray){
+
+    for (let i =0; i <stringArray.length; i++) {
+        const letter = stringArray[i];
+        const nextletter = stringArray[i+1];
+        if (letter == nextletter){
+            return true;
+        }
+    }
+    return false;
+}
+
+function disallowedCheck(stringArray){
+
+    for (let i =0; i <stringArray.length; i++) {
+        const letter = stringArray[i];
+        const nextletter = stringArray[i+1];
+        if (letter== 'a' && nextletter =='b'){
+            return false
+        }
+        if (letter == 'c' && nextletter=='d'){
+            return false
+        }
+        if (letter== 'p' && nextletter =='q'){
+            return false;
+        }
+        if (letter== 'x' && nextletter =='y'){
+            return false
+        }
+    }
+    return true;
+}
+
+function isNice(letters){
+
+    //const letters = stringArray.split('');
+
+    let vowel = vowelCheck(letters)
+    let repeat = repeatCheck(letters)
+    let disallow = disallowedCheck(letters)
+    console.log(vowel, repeat, disallow)
+    
+    return vowel && repeat && disallow
+}
+
+const input = readFile();
+//const input = "dvszwmarrgswjxmb"
+const inputArray = input.split('\n');
+let niceString = 0;
+let niceStringsTotal = 0;
+
+for (let i = 0; i < inputArray.length; i++) {
+    const isItNice = isNice(inputArray[i]);
+    if(isItNice){
+        niceStringsTotal += 1;}
+}
+console.log(niceStringsTotal);
